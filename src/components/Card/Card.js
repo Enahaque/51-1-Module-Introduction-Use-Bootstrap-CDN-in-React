ac43f7471dafedc38097891d535e63b8e266a79a
Enahaque <enahaque1164@gmil.com>
2022-06-20 20:23:28 +0600

    second commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Card = (props) => {
-    const{img,name,price}=props.product;
+    const{id,img,name,price}=props.product;
     return (
         
-             <div className="card">
+                <div className="card">
                     <img src={img} className="card-img-top" alt="..."/>
                         <div className="card-body">
+                            <h5 className="card-title">{id}</h5>
                             <h5 className="card-title">{name}</h5>
                             <p className="card-text">{price}</p>
                             <p className="card-text"><small className="text-muted">Newly arrival</small></p>
--- src/components/CardGroup/CardGroup.js
+++ src/components/CardGroup/CardGroup.js
@@ -3,10 +3,9 @@ import Card from '../Card/Card';
 
 const CardGroup = () => {
     const products = [
-        {id:1,img:'https://i.ibb.co/gdhrp92/joshua-woroniecki-lzh3h-Pt-Jz9c-unsplash.jpg', name:'Laptop-Pro',price:300000},
-        {id:2,img:'https://i.ibb.co/gdhrp92/joshua-woroniecki-lzh3h-Pt-Jz9c-unsplash.jpg', name:'Laptop-Gro',price:350000},
-        {id:3,img:'https://i.ibb.co/gdhrp92/joshua-woroniecki-lzh3h-Pt-Jz9c-unsplash.jpg', name:'Laptop-Bro',price:400000},
-       
+        {id:1,img:'https://i.ibb.co/sRn8y4q/image100.jpg', name:'Laptop-Pro',price:300000},
+        {id:2,img:'https://i.ibb.co/RD1vBKq/image101.jpg', name:'Laptop-Gro',price:350000},
+        {id:3,img:'https://i.ibb.co/m8Hq1Pw/image102.jpg', name:'Laptop-Bro',price:400000},
     ];
     return (
         <div>
