4b6cc5044d03dacfeaa0f1c4ce9033edc69074e4
Enahaque <enahaque1164@gmil.com>
2022-06-20 20:25:59 +0600

    3rd commit

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const Card = (props) => {
-    const{id,img,name,price}=props.product;
+    const{img,name,price}=props.product;
     return (
         
                 <div className="card">
                     <img src={img} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title">{id}</h5>
                             <h5 className="card-title">{name}</h5>
                             <p className="card-text">{price}</p>
                             <p className="card-text"><small className="text-muted">Newly arrival</small></p>
